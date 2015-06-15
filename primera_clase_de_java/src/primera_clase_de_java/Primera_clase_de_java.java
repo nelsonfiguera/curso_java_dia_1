126154e192c17d633ebb2004263605aef3f8e004
laboratorio <laboratorio@172.20.3.145>
2015-06-15 14:33:48 -0500

    cualquiera descricion  de modificasion q isite para q otra persona sepa donde modificaste

---
+++ primera_clase_de_java/src/primera_clase_de_java/Primera_clase_de_java.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primera_clase_de_java;
+
+/**
+ *
+ * @author laboratorio
+ */
+public class Primera_clase_de_java {
+
+    private int contador;
+    
+    
+    public static void main(String[] args) {
+        
+        Primera_clase_de_java jp1=new Primera_clase_de_java();
+        
+        // TODO code application logic he
+        for (jp1.contador= 0; jp1.contador< 5; jp1.contador++) {
+            System.out.println("imprimiento el contador \n\n          " +jp1.contador);
+           
+        }
+
+    }
+
+}
